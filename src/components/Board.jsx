954d8d4552470e6b775ff0057c208060cb2b26f4
Arhen Santiago <lincoldarhen9900@gmail.com>
2022-05-22 17:15:55 +0800

    opening spots done

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -11,9 +11,28 @@ const nearTiles = [
   [1, 0],
   [1, 1],
 ];
+const farTiles = [
+  [-2, -2],
+  [-2, -1],
+  [-2, 0],
+  [-2, 1],
+  [-2, 2],
+  [-1, -2],
+  [-1, 2],
+  [0, -2],
+  [0, 2],
+  [1, -2],
+  [1, 2],
+  [2, -2],
+  [2, -1],
+  [2, 0],
+  [2, 1],
+  [2, 2],
+];
 function Board({ tileCount }) {
   const [tiles, setTiles] = useState([]);
   const [isSelected, setIsSelected] = useState(false);
+  const [turn] = useState(1);
 
   const selectTile = (position) => {
     setTiles(
@@ -31,17 +50,62 @@ function Board({ tileCount }) {
     setTiles((prevTiles) =>
       prevTiles.map((row) =>
         row.map((tile) =>
-          tile.position === position ? { ...tile, near_spot: true } : tile
+          tile.position === position && !tile.marked
+            ? { ...tile, near_spot: true }
+            : tile
         )
+      )
+    );
+  };
+  const openFarSpots = (position) => {
+    setTiles((prevTiles) =>
+      prevTiles.map((row) =>
+        row.map((tile) =>
+          tile.position === position && !tile.marked
+            ? { ...tile, far_spot: true }
+            : tile
+        )
+      )
+    );
+  };
+  const playerMove = ({ near_spot, far_spot }) => {
+    if (near_spot) {
+      setTiles((prevTiles) =>
+        prevTiles.map((row) =>
+          row.map((tile) => {
+            return { ...tile, marked: true, team: turn };
+          })
+        )
+      );
+    }
+
+    if (far_spot) {
+      console.log("move");
+    }
+    setTiles(
+      tiles.map((row) =>
+        row.map((tile) => {
+          return {
+            ...tile,
+            near_spot: false,
+            far_spot: false,
+            selected: false,
+          };
+        })
       )
     );
   };
 
   const openSpots = () => {
     const nearSpots = findNeighboringTiles(tiles, nearTiles);
+    const farSpots = findNeighboringTiles(tiles, farTiles);
 
     nearSpots.forEach((spot) => {
       openNearSpots(spot);
+    });
+
+    farSpots.forEach((spot) => {
+      openFarSpots(spot);
     });
   };
 
@@ -64,7 +128,13 @@ function Board({ tileCount }) {
     >
       {tiles.map((row, i) =>
         row.map((col, j) => (
-          <Tile key={`${i}-${j}`} tile={col} selectTile={selectTile} />
+          <Tile
+            key={`${i}-${j}`}
+            tile={col}
+            selectTile={selectTile}
+            playerMove={playerMove}
+            turn={turn}
+          />
         ))
       )}
     </div>
@@ -76,14 +146,44 @@ function createBoard(size) {
   for (let i = 0; i < size; i++) {
     rows.push(
       Array.from(Array(size), (_, index) => {
-        return {
-          selected: false,
-          team: null,
-          marked: false,
-          position: `${i}-${index}`,
-          near_spot: false,
-          far_spot: false,
-        };
+        if (
+          (i === 0 && index === 0) ||
+          (i === 0 && index === 1) ||
+          (i === 0 && index === size - 2) ||
+          (i === 0 && index === size - 1)
+        ) {
+          return {
+            selected: false,
+            team: 2,
+            marked: true,
+            position: `${i}-${index}`,
+            near_spot: false,
+            far_spot: false,
+          };
+        } else if (
+          (i === size - 1 && index === 0) ||
+          (i === size - 1 && index === 1) ||
+          (i === size - 1 && index === size - 2) ||
+          (i === size - 1 && index === size - 1)
+        ) {
+          return {
+            selected: false,
+            team: 1,
+            marked: true,
+            position: `${i}-${index}`,
+            near_spot: false,
+            far_spot: false,
+          };
+        } else {
+          return {
+            selected: false,
+            team: null,
+            marked: false,
+            position: `${i}-${index}`,
+            near_spot: false,
+            far_spot: false,
+          };
+        }
       })
     );
   }
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,11 +1,21 @@
-function Tile({ selectTile, tile }) {
+function Tile({ selectTile, tile, turn, playerMove }) {
   return (
     <div
       className={`tile ${tile.selected ? "selected" : ""} ${
-        tile.near_spot || tile.far_spot ? "open-spot" : ""
-      }`}
-      onClick={() => selectTile(tile.position)}
-    ></div>
+        tile.near_spot ? "near" : ""
+      } ${tile.far_spot ? "far" : ""}`}
+      onClick={
+        tile.marked && turn === tile.team
+          ? () => selectTile(tile.position)
+          : tile.near_spot || tile.far_spot
+          ? () => playerMove(tile)
+          : null
+      }
+    >
+      {tile.marked ? (
+        <div className={`mark ${tile.team < 2 ? "team-1" : "team-2"}`}></div>
+      ) : null}
+    </div>
   );
 }
 
